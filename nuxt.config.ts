16eff8f0eb861a156e6242e21883491925fe5fa5
Mirage <clementbobin21@gmail.com>
2025-02-16 20:13:36 +0000

    feat: Add Scalar module for enhanced API documentation and configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,24 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  modules: ['@scalar/nuxt'],
   nitro: {
     experimental: {
       openAPI: true
     }
-  }
+  },
+  scalar: {
+    darkMode: true,
+    hideModels: false,
+    hideDownloadButton: false,
+    metaData: {
+      title: 'API Documentation by Scalar',
+    },
+    proxyUrl: 'https://proxy.scalar.com',
+    searchHotKey: 'k',
+    showSidebar: true,
+    pathRouting: {
+      basePath: '/docs',
+    },
+  },
 })
